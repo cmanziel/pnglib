2377d6ff6ac39b06f06ca7f70d5f8f0f158ef770
unknown <you@example.com>
2024-06-06 16:28:58 +0200

    function to search IDAT chunk signature in the file

--- pnglib.cpp
+++ pnglib.cpp
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #include "make_png.h"
 
+#include <string.h>
+
 #pragma warning (disable : 4996)
 
 uInt image_get_width(FILE* image)
@@ -49,13 +51,52 @@ uLong pixel_coords_to_index(FILE* image, unsigned int x, unsigned int y)
 	return (y + 1) + y * (image_get_width(image) * CHANNELS_PER_PIXEL) + (x * CHANNELS_PER_PIXEL);
 }
 
+unsigned int search_IDAT_start(FILE* image)
+{
+	const char* to_find = "IDAT\0";
+	char IDAT_sig[5];
+	unsigned int byte_num = 0;
+
+	// reset file pointer at the start in case it was previously moved by calls like fread
+	fseek(image, 0, SEEK_SET);
+
+	fread(IDAT_sig, sizeof(unsigned char), 4, image);
+
+	IDAT_sig[4] = '\0';
+
+	while (strcmp(to_find, IDAT_sig) != 0)
+	{
+		byte_num++;
+
+		// go back 3 bytes from the current pointer position to read bytes one after the other
+		fseek(image, -3, SEEK_CUR);
+
+		//cicle of freads until "IDAT" is found
+		fread(IDAT_sig, sizeof(unsigned char), 4, image);
+
+		IDAT_sig[4] = '\0';
+	}
+
+	return byte_num;
+}
+
 uLong image_get_IDAT_size(FILE* image)
 {
 	uLong idat_size;
 
-	// fseek to start of the IDAT chunk
-	// 33: first byte in the idat chunk
-	fseek(image, 33, SEEK_SET);
+	//// fseek to start of the IDAT chunk
+	//// 33: first byte in the idat chunk
+	//fseek(image, 33, SEEK_SET);
+	//// 4 bytes for the chunk's size's value
+	//fread(&idat_size, sizeof(unsigned char), 4, image);
+
+	unsigned int idat_start = search_IDAT_start(image);
+
+	fseek(image, idat_start, SEEK_SET);
+
+	// the length field start four bytes before the IDAT chunk type byte index
+	fseek(image, -4, SEEK_CUR);
+
 	// 4 bytes for the chunk's size's value
 	fread(&idat_size, sizeof(unsigned char), 4, image);
 
@@ -75,10 +116,11 @@ unsigned char* image_get_IDAT_data(FILE* image)
 		return NULL;
 	}
 
-	// fseek to start of the IDAT chunk data field
-	// 33: first byte in the idat chunk
-	// 4 bytes for size and 4 bytes for type: 41
-	fseek(image, 41, SEEK_SET);
+	unsigned int idat_start = search_IDAT_start(image);
+
+	// idat_start + 4: idat_start is the byte number for the first byte of teh chunk type field
+	// chuynk data start 4 bytes after it
+	fseek(image, idat_start + 4, SEEK_SET);
 
 	fread(idat_data, sizeof(unsigned char), idat_size, image);
 
@@ -99,35 +141,27 @@ FILE* create_image(unsigned char* data, const char* path, int width, int height)
 }
 
 // decomrpess the content of the image's IDAT (or IDATs) chunk
-unsigned char* decompress_image(const char* path)
+unsigned char* decompress_image(FILE* image)
 {
-	FILE* image = fopen(path, "rb");
-
 	uLong pixel_data_size = image_get_size(image);
 
-	uLong idat_size;
+	uLong idat_size = image_get_IDAT_size(image);
 
-	//// 33: first byte in the idat chunk
-	//fseek(image, 33, SEEK_SET);
-	//// 4 bytes for the chunk's size's value
-	//fread(&idat_size, sizeof(unsigned char), 4, image);
+	unsigned char* idat_data = image_get_IDAT_data(image);
 
-	idat_size = image_get_IDAT_size(image);
+	//// get idat data, send it to the decomp function
+	//unsigned char* idat_data = (unsigned char*)malloc(idat_size);
 
-	// get idat data, send it to the decomp function
-	unsigned char* idat_data = (unsigned char*)malloc(idat_size);
+	//if (idat_data == NULL)
+	//{
+	//	printf("decompressed data buffer not allocated\n");
+	//	return NULL;
+	//}
 
-	if (idat_data == NULL)
-	{
-		printf("decompressed data buffer not allocated\n");
-		return NULL;
-	}
-
-	fread(idat_data, sizeof(unsigned char), idat_size, image);
+	//fread(idat_data, sizeof(unsigned char), idat_size, image);
 
 	unsigned char* decomp_data = decomp(idat_data, pixel_data_size, idat_size);
 
-	fclose(image);
 	free(idat_data);
 
 	return decomp_data;
